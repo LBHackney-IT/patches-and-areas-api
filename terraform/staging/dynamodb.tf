4bc01bb94768f253d8a0c1b3abf6fd8c388479e5
humulla <43747248+humulla@users.noreply.github.com>
2021-11-05 16:17:39 +0000

    update dynamodb name

--- terraform/staging/dynamodb.tf
+++ terraform/staging/dynamodb.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "PatchesAndAreasApi_dynamodb_table" {
-  name           = "Patches"
+  name           = "PatchesAndAreas"
   billing_mode   = "PROVISIONED"
   read_capacity  = 10
   write_capacity = 10
